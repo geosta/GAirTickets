dc845917bbd14ca47790d146f116dc92f6fb73ce
george <it01151@uom.edu.gr>
2017-01-28 18:40:45 +0200

    initial

--- app/src/main/java/gr/gandg/george/gairtickets/MainActivity.java
+++ app/src/main/java/gr/gandg/george/gairtickets/MainActivity.java
@@ -3,6 +3,10 @@ package gr.gandg.george.gairtickets;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * UOM Mobile Development 2017 project
+ * (c) George Stathis - it01151
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
